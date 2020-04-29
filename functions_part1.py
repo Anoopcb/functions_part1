9699dca59180cd77c278578023d419146c462bbe
Anoop Singh <anoopcbjnu@gmail.com>
2020-04-29 12:09:23 +0100

    This is for functions basic examples

---
+++ functions_part1.py
@@ -0,0 +1,95 @@
+#### This is for functions
+
+def total_one(a, b):## function for adding two numbers
+
+    return a+b
+
+print(total_one(3, 5))
+
+
+##
+
+def total_two(num1, num2):
+    return num1 + num2
+
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+
+total = total_two(a, b)
+print(total)
+
+
+##
+a = str(input("enter first name : "))
+b = str(input("enter second name : "))
+
+total = total_two(a, b)
+print(total)
+
+## return vs print
+
+def total_three(a,b, c):
+
+    return a + b + c
+
+print(total_three(5, 5, 6))
+
+## you can write print just to have result
+def total_three(a, b, c):
+    print(a + b + c)
+total_three(5, 5, 6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
